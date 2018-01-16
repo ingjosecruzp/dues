61f98c658a27c05177fdee35e15039d5d67b166c
DenisseCastillo <34486303+DenisseCastillo@users.noreply.github.com>
2018-01-16 15:56:03 -0700

    Cambio en la función LargoCarta

--- www/controllers/ArticuloController.js
+++ www/controllers/ArticuloController.js
@@ -1,7 +1,6 @@
 app.controller('ArticuloController', function($scope,Usuario,articulo,$ionicPopup,$state,$stateParams,$rootScope,articulo) {
     $scope.data = {};
 	$scope.Articulo={};
-	$scope.Largo=490;
  
 	$scope.Inicio=function(){
 		$scope.Articulo=$rootScope.articulo;
@@ -28,17 +27,16 @@ app.controller('ArticuloController', function($scope,Usuario,articulo,$ionicPopu
 	 $scope.LargoCarta=function(tipo){
 
 		 if(tipo=='datosGenerales'){
-			 $scope.Largo= 850;
+			document.getElementById("Carta").style.height="850px";
 		 }
 		 else if(tipo=='datosVenta'){
-			 $scope.Largo= 1100;
+			document.getElementById("Carta").style.height="1100px";
 		 }
 		 else if(tipo=='datosCompra'){
-			 $scope.Largo= 700;
+			document.getElementById("Carta").style.height="700px";
 		 }
 		 else if(tipo=='listaPrecios'){
-			 console.log('entre a lista de precios');
-			$scope.Largo= 1100;
+			document.getElementById("Carta").style.height="1100px";
 		 }
 	 }
 });
