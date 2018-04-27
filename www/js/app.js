cf97adc5318d3d9c69ad98368ff6e81bd5e632bd
DenisseCastillo <34486303+DenisseCastillo@users.noreply.github.com>
2018-04-27 12:45:25 -0600

    lista de articulos

--- www/js/app.js
+++ www/js/app.js
@@ -168,7 +168,8 @@ app.run(function($ionicPlatform) {
 
 //Variables Globales
 app.value('Variables',{
-  IpServidor: '200.52.220.238:8082'
+  IpServidor: '192.168.0.102:8080'
+  //IpServidor: '200.52.220.238:8082'
   //IpServidor: '172.16.5.78:8080'
   //IpServidor: 'localhost:8080'
   //IpServidor: 'duestextil.fortiddns.com:8020/WcfDues'
